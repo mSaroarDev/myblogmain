efb5783616c3713915c3e61b426ab94c4e30cfdb
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2023-08-14 23:59:33 +0600

    coded

--- app/home/page.js
+++ app/home/page.js
@@ -1,14 +1,28 @@
 import Link from "next/link";
+import Post from "../components/Post";
+import blogPosts from "../data/blogData.json";
 
 export default function Homepage() {
+  const blogs = blogPosts.data;
+
   return (
-    <div className="h-screen w-full flex items-center justify-center">
-      <div className="text-center">
+    <main>
+      <div className="flex items-center justify-between">
         <h1 className="text-2xl font-bold">Welcome to News & Blogs World</h1>
-        <Link href="../blog">
-          <p className="font-bold mt-7">Explore Blog ⇾</p>
-        </Link>
+        <div className="">
+          <Link href="../blog">
+            <p className="font-bold">Explore Blog ⇾</p>
+          </Link>
+        </div>
       </div>
-    </div>
+
+      <div>
+        <div className="mt-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 justify-between mx-auto">
+          {blogs.map((post, index) => {
+            return <Post key={index} pmeter={post} />;
+          })}
+        </div>
+      </div>
+    </main>
   );
 }
