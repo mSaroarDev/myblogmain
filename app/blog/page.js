92052315479b7532ea2a1c6ab14bf06adc5a97d4
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2023-08-14 03:03:26 -0700

    json added

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,8 +1,21 @@
+"use client";
+import blogData from "../data/blogData.json";
+
 export default function BlogPage() {
+  const blogs = blogData.data;
+
   return (
     <div className="page">
       <main>
-        <h1>This is blog page</h1>
+        <h1>Blog Posts</h1>
+
+        {blogs.map((blog, index) => {
+          return (
+            <div key={index}>
+              <h1 className="font-bold mb-3">{blog.title}</h1>
+            </div>
+          );
+        })}
       </main>
     </div>
   );
